b99f0f39878ccc2c31737cd86f4b1a8af9c99c66
Gauthier Amano <gauthier.net@gmail.com>
2021-04-11 03:37:37 +0200

    fix(fetch): 🐛 use valid example to call `canal` middleware

--- src/middleware/test-async.js
+++ src/middleware/test-async.js
@@ -1,11 +1,12 @@
 (async () => {
-  const rawResponse = await fetch("http://localhost:3030/canal", {
+  // const rawResponse = await fetch("http://localhost:3030/canal", {
+  const rawResponse = await fetch("https://app.proj6ct.com/canal", {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ fichier: "bonjour", content: "au revoir" }),
+    body: JSON.stringify({ fichier: "bonjour", contenu: "au revoir" }),
   });
   const content = await rawResponse.json();
 
